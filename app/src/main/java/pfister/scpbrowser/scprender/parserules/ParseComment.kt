f2f9613f955cba1ad669053dad8e15bc6c508591
ej <EJPfister55@gmail.com>
2018-08-18 23:08:28 -0500

    Convenience method

--- app/src/main/java/pfister/scpbrowser/scprender/parserules/ParseComment.kt
+++ app/src/main/java/pfister/scpbrowser/scprender/parserules/ParseComment.kt
@@ -9,7 +9,7 @@ class ParseComment(override val text_engine: TextWikiEngine) : ParseDefault() {
     override val conf: Config? = null
 
     override fun process(match: MatchResult): CharSequence {
-        return text_engine.addToken("Comment",null)
+        return text_engine.addToken("Comment")
     }
 
 }
